e643b58033c48732d71bd35ea78780edf9956814
CrazyLab1001 <cramerkj@mail.uc.edu>
2025-02-01 14:11:07 -0500

    Task 2 complete!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double homeMaintenanceCost1 = 2838.38;
+        double homeMaintenanceCost2 = 1265.64;
+        double homeMaintenanceCost3 = 1284.19;
+        double homeMaintenanceCost4 = 3984.14;
+        double totalHomeMainenanceCost = homeMaintenanceCost1 + homeMaintenanceCost2 + homeMaintenanceCost3 + homeMaintenanceCost4;
+        System.out.println("For season 1, the home maintenance cost was " + homeMaintenanceCost1 + ". For season 2, the home maintenance cost was " + homeMaintenanceCost2 + ". For season 3, the home maintenance cost was " + homeMaintenanceCost3 + ". For season 4, the home maintenance cost was " + homeMaintenanceCost4 + ". Therefore, your total home maintenance cost is " + totalHomeMainenanceCost + ".");
     }
 }
